73bf16c1aacedb4b2801659a4b9fcccc7013adac
s3664099 <s3664099@student.rmit.edu.au>
2023-01-02 16:14:42 +1100

    Made battleship.py executable, and moved stuff into function

--- battleship.py
+++ battleship.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 #Add comments, and tidy code (if possible)
 #An extra line is being taken when the ships are placed
 #Have a check where if the value is invalid, then it is removed, and try again
@@ -10,25 +12,31 @@ import display_board
 import action
 import time
 
-#Creates the board for the player and the computer
-player = board.Board("Player")
-computer = board.Board("Computer")
 
-#Adds the ships to the board
-player.add_ships()
-computer.add_ships()
+def start_game():
+	#Creates the board for the player and the computer
+	player = board.Board("Player")
+	computer = board.Board("Computer")
 
-#Displays the boards
-display_board.display_grid(10,player.get_grid(),computer.get_grid())
+	#Adds the ships to the board
+	player.add_ships()
+	computer.add_ships()
 
-#Plays 20 rounds (Change to play until somebody wins)
-for x in range(20):
+	#Displays the boards
+	display_board.display_grid(10,player.get_grid(),computer.get_grid())
 
-	time.sleep(2)
-	print()
-	print("Computer")
-	action.fire(player,computer)
-	print("Player")
-	action.fire(computer,player)
-	print()
-	display_board.display_grid(10,player.get_spots_hit(),computer.get_spots_hit())
+	#Plays 20 rounds (Change to play until somebody wins)
+	for x in range(20):
+
+		time.sleep(2)
+		print()
+		print("Computer")
+		action.fire(player,computer)
+		print("Player")
+		action.fire(computer,player)
+		print()
+		display_board.display_grid(10,player.get_spots_hit(),computer.get_spots_hit())
+
+#Passes the current file as a module to the loader
+if __name__ == '__main__':
+	start_game()
